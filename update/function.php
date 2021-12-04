40b4a37ff7c7027c80bcabcb2b31e9fdc1b94ac1
alfattahdicky <alfattahdicky2@gmail.com>
2021-12-04 17:45:02 +0700

    Learn PHP: Update Form

---
+++ update/function.php
@@ -0,0 +1,80 @@
+<?php 
+  // Koneksi di baris pertama ke db
+  $db = mysqli_connect("localhost", "root", "", "phpdasar");
+
+  // Get/query data tabel person
+  function query($query) {
+    global $db;
+    $result = mysqli_query($db, $query);
+    $rows = [];
+
+    while($data = mysqli_fetch_assoc($result)) {
+      $rows[] = $data;
+    }
+
+    return $rows;
+  } 
+
+  function addQuery() {
+    global $db;
+
+    // Get Data Input & generate string
+    $image = htmlspecialchars($_POST['image']);
+    $name = htmlspecialchars($_POST['name']);
+    $age = htmlspecialchars($_POST['age']);
+    $city = htmlspecialchars($_POST['city']);
+
+    // Query Insert Data
+    $query = "INSERT INTO person
+              VALUES
+              (NULL, '$name', '$age', '$city', '$image')";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+  }
+
+  function deleteQuery($id) {
+    global $db;
+    mysqli_query($db, "DELETE FROM person WHERE id = $id");
+
+    return mysqli_affected_rows($db);
+  }
+
+  function updateQuery($id) {
+    global $db;
+
+    // Get Data Input & generate string
+    $image = htmlspecialchars($_POST['image']);
+    $name = htmlspecialchars($_POST['name']);
+    $age = htmlspecialchars($_POST['age']);
+    $city = htmlspecialchars($_POST['city']);
+
+    // Query Insert Data
+    $query = "UPDATE person SET 
+              nama='$name', 
+              age='$age',
+              city='$city', 
+              gambar='$image' WHERE id = $id
+            ";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+  }
+
+  // if(!$result) {
+  //   echo mysqli_error($db);
+  // } else {
+
+    // Get data (fetch) variable result
+    // mysqli_fetch_row() => return array numeric
+    // mysqli_fetch_assoc() => return array assosiative
+    // mysqli_fetch_array() => return array numeric dan associative
+    // mysql_fetch_object() => return object access with $data -> nama 
+
+    // while($fetchData = mysqli_fetch_assoc($result)) {
+    //   var_dump($fetchData);
+    // }
+
+?>
---
+++ update/ubah.php
@@ -0,0 +1,69 @@
+<?php
+  require 'function.php';
+
+  // Get data url 
+  $id = $_GET['id'];
+
+  // Query Data Person based on id 
+  // result array numeric
+  $person = query("SELECT * FROM person WHERE id = $id")[0];
+  
+
+  if(isset($_POST['submit'])) {
+    // Check added data update or failed
+    if(updateQuery($id) > 0) {
+      echo '
+        <script>
+          alert(\'Data berhasil diubah\');
+          document.location.href = \'index.php\';
+        </script>
+      ';
+    } else {
+      echo '
+        <script>
+          alert(\'Data gagal diubah\');
+          document.location.href = \'index.php\';
+        </script>
+      ';
+    }
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Update Data Person</title>
+</head>
+<body>
+  <h1>Update Data Person</h1>  
+
+  <form action="" method="POST">
+    <ul>
+      <li>
+        <label for="image">Gambar</label>
+        <input type="text" name="image" id="image" value="<?= $person['gambar'] ?>">
+      </li>
+      <li>
+        <label for="name">Nama</label>
+        <input type="text" name="name" id="name" value="<?= $person['nama'] ?>" required>
+      </li>
+      <li>
+        <label for="age">Umur</label>
+        <input type="text" name="age" id="age" value="<?= $person['age'] ?>" required>
+      </li>
+      <li>
+        <label for="city">Kota</label>
+        <input type="text" name="city" id="city" value="<?= $person['city'] ?>" required>
+      </li>
+      <li>
+        <button type="submit" name="submit">Update Data</button>
+      </li>
+    </ul>
+  </form>
+
+</body>
+</html>
