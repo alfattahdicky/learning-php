f0431994d6f3359d44c4cc08cee7e414bafa5e34
alfattahdicky <alfattahdicky2@gmail.com>
2021-12-06 20:18:24 +0700

    Learn PHP : Upload

---
+++ upload/function.php
@@ -0,0 +1,157 @@
+<?php 
+  // Koneksi di baris pertama ke db
+  $db = mysqli_connect("localhost", "root", "", "phpdasar");
+
+  // Get/query data tabel person
+  function query($query) {
+    global $db;
+    $result = mysqli_query($db, $query);
+    $rows = [];
+
+    while($data = mysqli_fetch_assoc($result)) {
+      $rows[] = $data;
+    }
+
+    return $rows;
+  } 
+
+  function addQuery() {
+    global $db;
+
+    // Get Data Input & generate string
+    $name = htmlspecialchars($_POST['name']);
+    $age = htmlspecialchars($_POST['age']);
+    $city = htmlspecialchars($_POST['city']);
+    
+    // upload image
+    $image = uploadImage();
+    if(!$image) {
+      return false;
+    }
+
+
+    // Query Insert Data
+    $query = "INSERT INTO person
+              VALUES
+              (NULL, '$name', '$age', '$city', '$image')";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+  }
+
+  function uploadImage() {
+    
+    // Get name, size, tmp & error for image
+    $nameFile = $_FILES['image']['name'];
+    $tmpFile = $_FILES['image']['tmp_name'];
+    $sizeFile = $_FILES['image']['size'];
+    $error = $_FILES['image']['error'];
+
+    // Check upload image empty or not
+    if($error === 4) {
+      echo '<script>
+              alert("Silahkan isi gambar");
+            </script>';
+      return false;
+    }
+
+    // Get extension image
+    $extensionImageValid = ['jpg', 'jpeg', 'png'];
+    //  One way
+    // $extensionImage = explode('.', $nameFile);
+    // $extensionImage = strtolower(end($extensionImage));
+    // Two way
+    $extensionImage = pathinfo($nameFile, PATHINFO_EXTENSION);
+
+    // Check image valid
+
+    // One Way
+    // for($i = 0; $i < count($extensionImageValid); $i++) {
+    //   if($extensionImage !== $extensionImageValid[$i]) {
+    //     echo '<script>
+    //           alert("File yang anda kirim salah");
+    //         </script>';
+    //     return false;
+    //   } else {
+
+    //   }
+    // }
+
+    // Two Way
+    if(!in_array($extensionImage, $extensionImageValid)) {
+      echo '<script>
+              alert("File yang anda kirim salah");
+            </script>';
+      return false;
+    }
+
+    // Check image size greater than 2mb
+    if($sizeFile > 1000000) {
+      echo '<script>
+              alert("File yang anda kirim sizenya lebih dari 2Mb");
+            </script>';
+      return false;
+    }
+
+    // Generate new name file image
+    $newNameFile = uniqid() . "." . $extensionImage;
+
+    // Upload image to directory folder img
+    move_uploaded_file($tmpFile, 'img/' . $newNameFile);
+
+    return $newNameFile;
+
+  }
+
+  function deleteQuery($id) {
+    global $db;
+    mysqli_query($db, "DELETE FROM person WHERE id = $id");
+
+    return mysqli_affected_rows($db);
+  }
+
+  function updateQuery($id) {
+    global $db;
+
+    // Get Data Input & generate string
+    $name = htmlspecialchars($_POST['name']);
+    $age = htmlspecialchars($_POST['age']);
+    $city = htmlspecialchars($_POST['city']);
+    
+    $person = query("SELECT * FROM person WHERE id = $id")[0];
+
+    if($_FILES['image']['error'] === 4) {
+      $image = $person['gambar'];
+    } else {
+      $image = uploadImage();
+    }
+    
+    // Query Insert Data
+    $query = "UPDATE person SET 
+              nama='$name', 
+              age='$age',
+              city='$city', 
+              gambar='$image' WHERE id = $id
+            ";
+
+    mysqli_query($db, $query);
+
+    return mysqli_affected_rows($db);
+  }
+
+  // if(!$result) {
+  //   echo mysqli_error($db);
+  // } else {
+
+    // Get data (fetch) variable result
+    // mysqli_fetch_row() => return array numeric
+    // mysqli_fetch_assoc() => return array assosiative
+    // mysqli_fetch_array() => return array numeric dan associative
+    // mysql_fetch_object() => return object access with $data -> nama 
+
+    // while($fetchData = mysqli_fetch_assoc($result)) {
+    //   var_dump($fetchData);
+    // }
+
+?>
